7ee8d260a2e90da2fdc2d7e36992124b1047d28b
Rob Elliott <elliott@iupui.edu>
2023-08-31 11:27:08 -0400

    Initial commit

--- 2_patterns.py
+++ 2_patterns.py
@@ -1,21 +1,13 @@
 import re
 
+"""
+Example: Find all of the filenames for image files (jpeg, jpg, bmp, gif, png).
+"""
+
 def main():
     filestring = "pretty.bmp, successful.json, international.mp3, office.docx, send.wav, small.webm, design.csv, smile.docx, food.wav, view.pdf, pull.js, cover.mp3, leader.numbers, value.flac, yourself.webm, four.mp3, if.wav, break.jpeg, example.xlsx, my.jpeg.json, former.mp4, at.txt, religious.css, mind.avi, babybmp.bmp, note.mp3, begin.flac, push.flac, later.gif, live.css, life.json, cover.flac, think.mp4, company.json, poor.png, present.odt, five.css, v.gif, happen.mp4, CASE.gif, T.gif, case.bmp, interesting.json, that.odt, rather.jpg, old.webm, generation.html, compare.mov, station.mp4, hand.js"
 
-    imagefiles = re.findall(r"\b[a-zA-Z]+\.(?:jpeg|jpg|bmp|gif|png)", filestring)
-    print(imagefiles)
 
-    # Progression to our more complex regular expression:
-        # jpeg
-        # \.jpeg
-        # \.(jpeg|jpg)
-        # \.(jpeg|jpg|bmp|gif|png)
-        # \.(?:jpeg|jpg|bmp|gif|png)
-        # [a-z]\.(?:jpeg|jpg|bmp|gif|png)
-        # \b[a-z]\.(?:jpeg|jpg|bmp|gif|png)
-        # \b[a-z]+\.(?:jpeg|jpg|bmp|gif|png)
-        # \b[a-zA-Z]+\.(?:jpeg|jpg|bmp|gif|png)
 
 if __name__== "__main__":
   main()
--- 3_patterns_list.py
+++ 3_patterns_list.py
@@ -1,22 +1,11 @@
 import re
 
+"""
+Example: Find all files that are the OLD MS Office type (xls, doc, ppt) from a list of files using list comprehension
+"""
+
 def main():
     filelist = ['music.xls', 'international.ppt', 'arrive.ppt', 'tough.ppt', 'service.ppt', 'along.pdf', 'per.odt', 'without.numbers', 'stop.numbers', 'life.pdf', 'maintain.pdf', 'yard.docx', 'foot.xlsx', 'as.pages', 'TYPE.DOC', 'check.docx', 'bar.odp', 'life.key', 'return.xlsx', 'explain.ppt', 'clearly.ppt', 'nation.pdf', 'concern.numbers', 'continue.numbers', 'pattern.odt', 'executive.pdf', 'culture.numbers', 'wrong.doc', 'generation.docx', 'that.pdf', 'womaN.XLS', 'eight.ppt', 'politics.odp', 'growth.odp', 'far.odt', 'them.key', 'since.ppt', 'least.numbers', 'rest.docx', 'anyone.numbers', 'happen.key', 'my.key', 'a.ods', 'new.odp', 'traditional.pptx', 'responsibility.numbers', 'employee.odp', 'picture.doc', 'keep.xlsx', 'believe.doc']
-
-    # Progression to our more complex regular expression:
-        # xls|doc|ppt
-        # \.(xls|doc|ppt)
-        # \.(?:xls|doc|ppt)
-        # \.(?:xls|doc|ppt)\b
-        # [a-z]\.(?:xls|doc|ppt)\b
-        # [a-z0-9]\.(?:xls|doc|ppt)\b
-        # [a-z0-9]+\.(?:xls|doc|ppt)\b
-
-    oldfiles = []
-
-    oldfiles = [f for f in filelist if re.search(r"[a-z0-9]+\.(?:xls|doc|ppt)\b", f, re.IGNORECASE)]
-
-    print(oldfiles)
 
 if __name__== "__main__":
   main()
